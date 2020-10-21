6816c836cc463e92144b4ffa03a4730e2abec044
Mikael Rortais <mro.gith@gmail.com>
2020-10-22 00:24:41 +0200

    new-B

---
+++ .history/src/app/main_20201022003142.ts
@@ -0,0 +1,7 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+console.log("aaaa")
+//TODO
+platformBrowserDynamic().bootstrapModule(AppModule);
+//TODO 
