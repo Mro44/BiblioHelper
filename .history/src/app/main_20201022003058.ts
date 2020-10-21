2b855e70a0c83939de65e23c372af60fe620b4a6
Mikael Rortais <mro.gith@gmail.com>
2020-10-22 00:24:41 +0200

    rebase interactive

--- .history/src/app/main_20201022003058.ts
+++ .history/src/app/main_20201022003058.ts
@@ -2,6 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 console.log("aaaa")
+=======
 //TODO
+>>>>>>> new-B
 platformBrowserDynamic().bootstrapModule(AppModule);
 //TODO 
--- .history/src/app/main_20201022003100.ts
+++ .history/src/app/main_20201022003100.ts
@@ -3,5 +3,6 @@ import { AppModule } from './app.module';
 
 console.log("aaaa")
 //TODO
+>>>>>>> new-B
 platformBrowserDynamic().bootstrapModule(AppModule);
 //TODO 
