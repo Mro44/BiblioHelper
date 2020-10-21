11ae7a9df9e860bb96e3110a64175a534e72b301
Mikael Rortais <mro.gith@gmail.com>
2020-10-22 00:29:43 +0200

    master-C

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,5 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
+console.log("aaaa")
 platformBrowserDynamic().bootstrapModule(AppModule);
 //TODO 
